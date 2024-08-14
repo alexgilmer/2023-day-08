0c9f766446835d4ab68a262654f4bf3ac8130778
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-08-14 16:21:15 -0500

    Solve part 1

---
+++ 2023-day-08/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_day_08;
+
+public class Node(string id, string left, string right)
+{
+    public string Id = id;
+    public string Left = left;
+    public string Right = right;
+}
--- 2023-day-08/Program.cs
+++ 2023-day-08/Program.cs
@@ -4,10 +4,37 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        bool testMode = true;
+        bool testMode = false;
         IList<string> input = testMode ? GetTestInput() : GetPuzzleInput();
 
+        string instructionList = input[0];
 
+        Dictionary<string, Node> nodeList = new();
+        for (int i = 2; i < input.Count; i++)
+        {
+            string id = input[i][0..3];
+            string left = input[i][7..10];
+            string right = input[i][12..15];
+            nodeList.Add(id, new Node(id, left, right));
+        }
+
+        string curLocation = "AAA";
+        int stepCount = 0;
+        while (curLocation != "ZZZ" && stepCount < 1_000_000)
+        {
+            int instruction = stepCount % instructionList.Length;
+            char direction = instructionList[instruction];
+
+            Node curNode = nodeList[curLocation];
+            curLocation = direction switch
+            {
+                'R' => curNode.Right,
+                'L' => curNode.Left,
+                _ => throw new InvalidOperationException(),
+            };
+            stepCount++;
+        }
+        Console.WriteLine(stepCount);
     }
 
     static IList<string> GetPuzzleInput()
@@ -26,13 +53,8 @@ internal class Program
 
     static IList<string> GetTestInput()
     {
-        return [
-            "32T3K 765",
-            "T55J5 684",
-            "KK677 28",
-            "KTJJT 220",
-            "QQQJA 483"
-            ];
+        // not doing test inputs for this one
+        throw new NotImplementedException();
     }
 
 }
